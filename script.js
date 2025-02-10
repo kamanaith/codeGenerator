7b87c8cc8a3a0bdebb2af0ebb1a792d711f6d5cc
kamanaith <timsinakamaksha@gmail.com>
2025-02-10 11:13:52 +0545

    symbols added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let numbers = [0,1,2,3,4,5,6,7,8,9];
+let symbols = ["!","@","#","$","&"]
 
 // let btn = document.querySelector(".generator");
 let displayer = document.querySelector(".container");
@@ -18,7 +19,8 @@ let result = [
     getrandomElement(lowerCase),
     getrandomElement(lowerCase),
     getrandomElement(numbers),
-    getrandomElement(numbers)
+    getrandomElement(numbers),
+    getrandomElement(symbols)
 ]
 
 result = result.sort(() => Math.random()-0.5);
